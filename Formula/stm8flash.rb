ce6ee36b3d848d3a1c4fab52559cf33beb70196c
Qi Wenmin <qiwenmin@gmail.com>
2018-03-29 12:14:14 +0800

    Updated to the newest commit

--- Formula/stm8flash.rb
+++ Formula/stm8flash.rb
@@ -1,8 +1,8 @@
 class Stm8flash < Formula
   desc "Program your stm8 devices with SWIM/stlinkv(1,2)"
   homepage "https://github.com/vdudouyt/stm8flash"
-  url "https://github.com/vdudouyt/stm8flash.git", :revision => "6007cb7062bb86d4c17c79174ee177e80939f602"
-  version "20170616-1.1"
+  url "https://github.com/vdudouyt/stm8flash.git", :revision => "11728cc64f180fe24e087dc0adccbc46291fe085"
+  version "20170616-1.1-11728cc"
   depends_on "libusb"
   depends_on "pkg-config" => :build
   def install
