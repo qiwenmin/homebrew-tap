eaaefb7428141be778f34bf22bd9b709062961a5
Qi Wenmin <qiwenmin@gmail.com>
2018-11-28 11:58:15 +0800

    Upgrade stm8flash to commit-50a253d

--- Formula/stm8flash.rb
+++ Formula/stm8flash.rb
@@ -1,8 +1,8 @@
 class Stm8flash < Formula
   desc "Program your stm8 devices with SWIM/stlinkv(1,2)"
   homepage "https://github.com/vdudouyt/stm8flash"
-  url "https://github.com/vdudouyt/stm8flash.git", :revision => "11728cc64f180fe24e087dc0adccbc46291fe085"
-  version "20170616-1.1-11728cc"
+  url "https://github.com/vdudouyt/stm8flash.git", :revision => "50a253df88afca556acf3130706fd525b5ec48f8"
+  version "20170616-1.1-50a253d"
   depends_on "libusb"
   depends_on "pkg-config" => :build
   def install
