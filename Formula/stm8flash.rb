e46c5d4cad5ce7447fd9dccf9717d43c0e0765e1
Qi Wenmin <qiwenmin@gmail.com>
2019-12-28 22:09:33 +0800

    Upgrade stm8flash to 3ad9500.

--- Formula/stm8flash.rb
+++ Formula/stm8flash.rb
@@ -1,8 +1,8 @@
 class Stm8flash < Formula
   desc "Program your stm8 devices with SWIM/stlinkv(1,2)"
   homepage "https://github.com/vdudouyt/stm8flash"
-  url "https://github.com/vdudouyt/stm8flash.git", :revision => "50a253df88afca556acf3130706fd525b5ec48f8"
-  version "20170616-1.1-50a253d"
+  url "https://github.com/vdudouyt/stm8flash.git", :revision => "3ad9500a04c21f08182439425b19d900c2315217"
+  version "20170616-1.1-3ad9500"
   depends_on "libusb"
   depends_on "pkg-config" => :build
   def install
