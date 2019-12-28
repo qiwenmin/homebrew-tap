96d2e42b8df0259688d38cd4a8b3ea86b35af90b
Qi Wenmin <qiwenmin@gmail.com>
2019-12-28 22:29:47 +0800

    Upgrade micronucleus to 2.04.

--- Formula/micronucleus.rb
+++ Formula/micronucleus.rb
@@ -1,9 +1,9 @@
 class Micronucleus < Formula
   desc "Strong emphasis on bootloader compactness ATTiny usb bootloader"
   homepage "https://github.com/micronucleus/micronucleus"
-  url "https://github.com/micronucleus/micronucleus/archive/v2.03.tar.gz"
-  version "2.03"
-  sha256 "a56079018f5b7ce6e4916fc8f99ff5db5d888539f3622400009a79fa3beba2ce"
+  url "https://github.com/micronucleus/micronucleus/archive/2.04.tar.gz"
+  version "2.04"
+  sha256 "e09d4e427cde4dac43c3bac2e6f91903ae205263b1598beb8cef4f786692b506"
   depends_on "libusb-compat"
   def install
     system "make", "-C", "commandline", "-j1"
